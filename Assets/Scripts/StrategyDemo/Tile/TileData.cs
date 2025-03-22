7a50c35384d1161ae2442538612c026d95cbe59a
Fatih Aslan <44754892+fatihaslaan@users.noreply.github.com>
2025-03-22 21:46:35 +0300

    Bool addition to TileData for making it movable and constructable

--- Assets/Scripts/StrategyDemo/Tile/TileData.cs
+++ Assets/Scripts/StrategyDemo/Tile/TileData.cs
@@ -7,8 +7,12 @@ namespace StrategyDemo.Tile_NS
     {
         [SerializeField] private TileType _tileType;
         [SerializeField] private Sprite _sprite;
+        [SerializeField] private bool _isMovable = true;
+        [SerializeField] private bool _isConstructable = true;
 
         public TileType TileType { get { return _tileType; } }
         public Sprite Sprite { get { return _sprite; } }
+        public bool IsMovable { get { return _isMovable; } }
+        public bool IsConstructable { get { return _isConstructable; } }
     }
 }
