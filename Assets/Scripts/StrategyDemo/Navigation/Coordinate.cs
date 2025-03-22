8bd43536432c1154badf728420727c5062a73089
Fatih Aslan <44754892+fatihaslaan@users.noreply.github.com>
2025-03-22 21:46:48 +0300

    Coordinate Refactor

--- Assets/Scripts/StrategyDemo/Navigation/Coordinate.cs
+++ Assets/Scripts/StrategyDemo/Navigation/Coordinate.cs
@@ -1,9 +1,47 @@
+using System.Collections.Generic;
+using System.Numerics;
+using UnityEngine;
+
 namespace StrategyDemo.Navigation_NS
 {
     [System.Serializable]
     public class Coordinate
     {
-        public int xCoorddinate;
+        public int xCoordinate;
         public int yCoordinate;
+
+        public Coordinate(int x, int y)
+        {
+            xCoordinate = x;
+            yCoordinate = y;
+        }
+
+        public HashSet<(int x, int y)> GetNeighbourCoordinates()
+        {
+            HashSet<(int x, int y)> neighbors = new();
+            Vector2Int[] neighborCoordinates =
+            {
+                new Vector2Int(-1,-1),
+                new Vector2Int(1,-1),
+                new Vector2Int(1,1),
+                new Vector2Int(-1,1),
+                new Vector2Int(-1,0),
+                new Vector2Int(1,0),
+                new Vector2Int(0,-1),
+                new Vector2Int(0,1),
+            };
+
+            foreach (Vector2Int coordinate in neighborCoordinates)
+            {
+                neighbors.Add((xCoordinate + coordinate.x, yCoordinate + coordinate.y));
+            }
+
+            return neighbors;
+        }
+
+        public bool IsOffset()
+        {
+            return Mathf.Abs(xCoordinate) % 2 != Mathf.Abs(yCoordinate) % 2;
+        }
     }
 }
--- Assets/Scripts/StrategyDemo/Navigation/TileCoordinate.cs
+++ Assets/Scripts/StrategyDemo/Navigation/TileCoordinate.cs
@@ -1,17 +1,28 @@
 using StrategyDemo.Tile_NS;
+using System.Collections.Generic;
 
 namespace StrategyDemo.Navigation_NS
 {
     [System.Serializable]
-    public class TileCoordinate:Coordinate
+    public class TileCoordinate : Coordinate
     {
         public TileData tileData;
 
-        public TileCoordinate(int x, int y, TileData tileData)
+        public TileCoordinate(int x, int y, TileData tileData) : base(x, y)
         {
-            xCoorddinate = x;
-            yCoordinate = y;
             this.tileData = tileData;
+        }
+
+        public TileCoordinate(Coordinate coordinate, TileData tileData) : base(coordinate.yCoordinate, coordinate.yCoordinate)
+        {
+            this.tileData = tileData;
+        }
+
+        public List<TileCoordinate> GetNeighbourCoordinates(List<TileCoordinate> tiles)
+        {
+            HashSet<(int x, int y)> neighborCoordinates = GetNeighbourCoordinates();
+
+            return tiles.FindAll(tileCoordinate => neighborCoordinates.Contains((tileCoordinate.xCoordinate, tileCoordinate.yCoordinate)));
         }
     }
 }
